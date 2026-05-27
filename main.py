c71d5f336f2cad41983e95a27b435e29a0959d84
radhekrishnan18275-commits <radhekrishnan18275@gmail.com>
2026-05-27 11:23:10 +0530

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import time
+import requests
+
 BOT_TOKEN = "8954212814:AAHGIp4mxbKbFHn70uulbXGRNcy1ROJhCm0"
 CHAT_ID = "8241640506"
+
+def send_message(msg):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        }
+    )
+
+print("BOT STARTED SUCCESSFULLY")
+
+send_message("✅ BOT IS ONLINE")
+
+while True:
+
+    print("BOT RUNNING...")
+
+    time.sleep(30)
